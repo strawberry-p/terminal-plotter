4999c714b6a23b425fee5fdbf459f701739a4642
strawberry-p <hatch.lol@seznam.cz>
2025-06-21 20:53:49 +0200

    Newer version (adds cmdline options)

--- termgraph.py
+++ termgraph.py
@@ -1,6 +1,7 @@
 import math
 import os
 import time
+import argparse
 yVals = [[20,10,50,40,100]]
 yMax = max(yVals[0])
 XDIST = 50 #available space for graph
@@ -9,66 +10,129 @@ YDIST = 39
 FULLCHAR = "#"
 horizontalSeparator = "="
 renderTable = []
-elementGap = round((XDIST/2)/(len(yVals[0])))
+elementGap = 5
 gapPlace = []
 xLabels = []
-yLabels = [0, yMax]
-yLabelPos = [YDIST, 0]
+yLabels = []
+yLabelPos = []
 yLabelLength = 0
+fileSeparator = ","
+debugLevel = 3 #set to 7 when long_write errors
+customXLabels = False
+xLabelShift = 0
+def load_csv(filename):
+    global yVals
+    with open(filename) as file:
+        loadCheck = 0
+        for line in file:
+            iLoad = 0
+            yVals.append([])
+            for item in line.split(fileSeparator):
+                yVals[-1].append(float(item.strip())) #append to last dataset array
+                iLoad += 1
+            if debugLevel > 3: #debug
+                if loadCheck < 1:
+                    loadCheck = iLoad
+                elif loadCheck == iLoad:
+                    iLoad = 0 #reset loading counter
+                else:
+                    print(f"expected {loadCheck} got {iLoad} at line {line}")
+                    raise ValueError(f"err when loading csv lines, last {yVals[-2]} now {yVals[-1]}")
+
 def y_check(valArray):
     countArray = []
     for dataArray in valArray:
         countArray.append(len(dataArray))
     sortedCountArray = sorted(countArray)
-    print(countArray)
     if sortedCountArray[0] == sortedCountArray[-1]:
         return True
     else:
         return False
+def operation_2D(array2D,op=max):
+    opArray = []
+    for array1D in array2D:
+        opArray.append(op(array1D))
+    return (op(opArray),max(opArray),len(opArray))
 def reverseNorm(value,mode=0,base=10):
     if mode == 1:
         return math.sqrt(value*yMax**2)
     else:
         return value*yMax
 def count_gap_place():
-    global gapPlace
+    global gapPlace, elementGap
+    elementGap = math.floor((XDIST/2)/(len(yVals[0])))
     for i in range(len(yVals[0])):
         gapPlace.append(elementGap+elementGap*i*2)
         #comment out if using custom labels:
-        xLabels.append(10*i)
+        if False == customXLabels:
+            xLabels.append(i)
+    if gapPlace[-1] > XDIST-2:
+        gapPlace[-1] = XDIST-2
+    if debugLevel > 2:
+        print(gapPlace)
+        print(elementGap)
+        time.sleep(1)
 def make_y_label(fraction=4):
     global yLabels, yLabelLength, yLabelPos
+    yLabels.append(round(yMax))
+    yLabelPos.append(0)
     i = 1
     unitFrac = 1/fraction
-    while i < fraction-1:
+    while i < fraction:
         yLabels.append(round(reverseNorm(unitFrac*i)))
-        yLabelPos.append(round(YDIST*unitFrac*i))
+        yLabelPos.append(round(YDIST*(1-unitFrac*i)))
         i += 1
     lengthDecider = []
-    for i in yLabelPos:
+    for i in yLabels:
         lengthDecider.append(len(str(i)))
-    yLabelLength = sorted(lengthDecider)[-1]+1
-    print(f"length {yLabelLength} labels {yLabels} label pos {yLabelPos}")
-if __name__ == "__main__":
+    yLabelLength = sorted(lengthDecider)[-1]
+    if debugLevel > 5:
+        print(f"length {yLabelLength} labels {yLabels} label pos {yLabelPos}")
+def halving(array,mult=0.5):
+    i = 0
+    while i < len(array):
+        array[i] = float(array[i])*mult
+        i += 1
+def termgraph_prepare():
+    global yVals,xLabelShift,yMax,XDIST,YDIST
+    parser = argparse.ArgumentParser(description="CLI graph rendering")
+    parser.add_argument("file", type=str, help="comma-separated values to plot, one set per each line")
+    parser.add_argument("--y-label-fraction",type=int,default=4,help="Number of labels on the y axis")
+    parser.add_argument("--y-space",default=39,help="Character count for graph height")
+    parser.add_argument("--x-space",default=50,help="Character count for graph width")
+    args = parser.parse_args()
+    if args.file != "" and args.file != None :
+        yVals = []
+        load_csv(args.file)
+        if debugLevel > 5:
+            print(yVals)
+    #if debugLevel > 2:
+    #    for dataset in yVals:
+    #        halving(dataset,1)
+    xLabelShift = math.floor(len(yVals)/2)
+    yMax = operation_2D(yVals,max)[0]
+    XDIST = round(int(args.x_space))
+    YDIST = round(int(args.y_space))
     count_gap_place()
-    make_y_label()
-    #print(gapPlace)
-    #time.sleep(2)
+    make_y_label(args.y_label_fraction)
+if __name__ == "__main__":
+    termgraph_prepare()
 def long_write(input, xLastPos, yPos=len(renderTable)-1):
     strInput = str(input)
-    print(f"input {strInput} y {yPos}")
+    if debugLevel > 6:
+        print(f"input {strInput} y {yPos}")
     firstX = xLastPos-len(strInput)+1
     if firstX >= 0:
         for i in range(len(strInput)):
-            print(renderTable[firstX+i])
-            print(f"y {yPos} firstX {firstX} i {i} last len {len(renderTable[firstX+i-1])} len {len(renderTable[firstX+i])}")
+            if debugLevel > 6:
+                print(renderTable[firstX+i])
+                print(f"y {yPos} firstX {firstX} i {i} last len {len(renderTable[firstX+i-1])} len {len(renderTable[firstX+i])}")
             if renderTable[firstX+i][yPos] == " ":
                 
                 
                 renderTable[firstX+i][yPos] = strInput[i]
             else:
                 print(f"x {firstX+i} letter {strInput[i]} would replace {renderTable[firstX+i][yPos]}")
-                time.sleep(1)
     else:
         print(f"{input} ending at {xLastPos} with y {yPos} too long")
         time.sleep(2)
@@ -100,8 +164,8 @@ def init_render_table(xdist, ydist,nullchar=" ", yLabelOffset=0):
                 renderTable[i].append(horizontalSeparator)
             elif j == ydist-1:
                 if (i in gapPlace):
-                    renderTable[i].append(" ") #long_write can not append
-                    long_write(xLabels[gapPlace.index(i)],xLastPos=i,yPos=j)
+                    renderTable[i].append(nullchar) #long_write can not append
+                    long_write(xLabels[gapPlace.index(i)],xLastPos=i+xLabelShift,yPos=j)
                 else:
                     renderTable[i].append(nullchar)
             else:
@@ -134,8 +198,6 @@ def d_slope(height):
     print(targetTan)
     return 180*math.atan(targetTan)/math.pi #return value in degrees
 def bar_graph(vals=yVals[0],offset=0,xdist=XDIST,ydist=YDIST, gap=elementGap):
-    global yMax
-    yMax = max(vals)
     gapPos = gap+offset
     for iVal in range(len(vals)):
         iterVal = vals[iVal]
@@ -145,7 +207,7 @@ def bar_graph(vals=yVals[0],offset=0,xdist=XDIST,ydist=YDIST, gap=elementGap):
             i -= 1
             renderTable[gapPos][i] = FULLCHAR
         gapPos += 2*gap #"gap" is half the intended gap
-if __name__ == "__main__":
+def termgraph_render():
     init_render_table(XDIST,YDIST,yLabelOffset=yLabelLength)
     if y_check(yVals):
         iOffset = 0
@@ -157,3 +219,5 @@ if __name__ == "__main__":
         time.sleep(1)
         bar_graph(yVals[0], offset=0)
     write_render_table()
+if __name__ == "__main__":
+    termgraph_render()
